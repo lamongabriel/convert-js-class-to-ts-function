99ee0679dc0a1b3b0b5e59588d5e6db634bcb2cd
lamongabriel <gabriel-lamon@outlook.com>
2022-12-21 15:12:45 -0300

    Function component: Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import { FiPlusSquare } from 'react-icons/fi';
 
 import { Container } from './styles';
@@ -8,31 +7,27 @@ interface HeaderProps {
   openModal: () => void
 }
 
-class Header extends Component<HeaderProps> {
-  render() {
-    const { openModal } = this.props;
-
-    return (
-      <Container>
-        <header>
-          <img src={Logo} alt="GoRestaurant" />
-          <nav>
-            <div>
-              <button
-                type="button"
-                onClick={openModal}
-              >
-                <div className="text">Novo Prato</div>
-                <div className="icon">
-                  <FiPlusSquare size={24} />
-                </div>
-              </button>
-            </div>
-          </nav>
-        </header>
-      </Container>
-    )
-  }
+export function Header ({ openModal }: HeaderProps) {
+  return (
+    <Container>
+      <header>
+        <img src={Logo} alt="GoRestaurant" />
+        <nav>
+          <div>
+            <button
+              type="button"
+              onClick={openModal}
+            >
+              <div className="text">Novo Prato</div>
+              <div className="icon">
+                <FiPlusSquare size={24} />
+              </div>
+            </button>
+          </div>
+        </nav>
+      </header>
+    </Container>
+  )
 };
 
 export default Header;
